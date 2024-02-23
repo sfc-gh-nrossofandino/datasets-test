4ee4653924bea14c901dd7adeae7b7a4a9ab5128
Nahuel Emiliano Rosso Fandiño <n.rossofandino@snowflake.com>
2024-02-23 21:47:10 +0000

    changes

--- Hello.py
+++ Hello.py
@@ -1,51 +1,12 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import streamlit as st
-from streamlit.logger import get_logger
-
-LOGGER = get_logger(__name__)
-
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-
-if __name__ == "__main__":
-    run()
+conn = st.connection("snowflake")
+conn.cursor().execute('use database FREE_DATASET_GZTSZAS2KH9')
+query = conn.query("""SELECT i.cik, i.company_name, r.period_start_date, r.period_end_date, r.measure_description, TO_NUMERIC(r.value) AS value
+FROM cybersyn.sec_cik_index AS i
+JOIN cybersyn.sec_report_attributes AS r ON (r.cik = i.cik)
+WHERE i.sic_code_description = 'AIR TRANSPORTATION, SCHEDULED'
+  AND r.statement = 'Income Statement'
+  AND r.period_end_date = '2022-12-31'
+  AND r.covered_qtrs = 4
+  AND r.metadata IS NULL
+  AND r.measure_description IN ('Total operating revenues', 'Total operating revenue');""")
+st.dataframe(query)
